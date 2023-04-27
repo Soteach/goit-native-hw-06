75dc08d4cec13dcebb73fd202e3204afe69f97aa
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-04-27 14:45:11 +0300

    add and tune redux

--- App.js
+++ App.js
@@ -1,60 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { useRoute } from './router';
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 export default function App() {
   const routing = useRoute(true);
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{routing}</NavigationContainer>
+    </Provider>
+  );
 }
-
-// const AuthStack = createNativeStackNavigator();
-// const MainTab = createBottomTabNavigator();
-
-// const useRoute = isAuth => {
-//   if (!isAuth) {
-//     <AuthStack.Navigator>
-//       <AuthStack.Screen
-//         options={{ headerShown: false }}
-//         name={'Login'}
-//         component={LoginScreen}
-//       />
-//       <AuthStack.Screen
-//         name={'Register'}
-//         component={RegisterScreen}
-//         options={{ headerShown: false }}
-//       />
-//     </AuthStack.Navigator>;
-//   }
-//   return (
-//     <MainTab.Navigator>
-//       <MainTab.Screen name={'Posts'} component={PostsScreen} />
-//       <MainTab.Screen name={'Create'} component={CreatePostsScreen} />
-//       <MainTab.Screen name={'Profile'} component={ProfileScreen} />
-//     </MainTab.Navigator>
-//   );
-// };
-
-// export default function App() {
-//   const [iasReady, setIasReady] = useState(false);
-//   const routing = useRoute(null);
-
-//   if (!iasReady) {
-//     return (
-//       <AppLoading
-//         startAsync={loadApplication}
-//         onFinish={() => setIasReady(true)}
-//         onError={console.warn}
-//       />
-//     );
-//   }
-
-//   return (
-//     <>
-//       <NavigationContainer>{routing}</NavigationContainer>
-//       {/* <ProfileScreen /> */}
-//       {/* <CommentsScreen /> */}
-//       {/* <PostsScreen /> */}
-//       {/* <CreatePostsScreen /> */}
-//       {/* <Home /> */}
-//     </>
-//   );
-// }
