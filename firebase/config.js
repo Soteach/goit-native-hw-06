4bfec0e700301f64176903f9657fd5669a610a41
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-04-27 15:00:29 +0300

    tune firebase and add config

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCs1kfB1xWA7Mi1KtBrooFWNnGnRc3W500',
+  authDomain: 'rn-social-a1f1e.firebaseapp.com',
+  projectId: 'rn-social-a1f1e',
+  storageBucket: 'rn-social-a1f1e.appspot.com',
+  messagingSenderId: '502110576966',
+  appId: '1:502110576966:web:18e23fba47caa2738f4547',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
